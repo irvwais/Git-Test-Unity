d79f369b748bcf8d830da5ed916e9401245ff78e
Irving Waisman <irving.m.waisman@gmail.com>
2020-09-22 16:30:02 -0400

    Test Small Changes

---
+++ GitHubTestProject/Assets/Scripts/BoxBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script Written by Irv Wais
+
+public class BoxBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
